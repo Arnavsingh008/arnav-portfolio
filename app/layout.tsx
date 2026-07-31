b4b6175018454007fc3e8ff73d97f6c068a03893
Arnav Singh <arnavsinghh08@gmail.com>
2026-07-31 20:19:29 +0530

    Improve favicon support

--- app/layout.tsx
+++ app/layout.tsx
@@ -78,10 +78,26 @@ export const metadata: Metadata = {
   },
 
   icons: {
-    icon: "/favicon.jpg",
-    shortcut: "/favicon.jpg",
-    apple: "/favicon.jpg",
-  },
+  icon: [
+    {
+      url: "/favicon.ico",
+    },
+    {
+      url: "/favicon-16x16.png",
+      sizes: "16x16",
+      type: "image/png",
+    },
+    {
+      url: "/favicon-32x32.png",
+      sizes: "32x32",
+      type: "image/png",
+    },
+  ],
+  shortcut: "/favicon.ico",
+  apple: "/apple-touch-icon.png",
+},
+
+manifest: "/site.webmanifest",
 
   openGraph: {
     title: "Arnav Singh | Full Stack MERN Developer",
